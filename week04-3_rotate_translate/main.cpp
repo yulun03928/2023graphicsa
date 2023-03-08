cc42fb0b637716e66a989ccd7ad067697b06f27a
yulun03928 <yulun03928@gmail.com>
2023-03-08 11:58:49 +0800

    add week04

---
+++ week04-3_rotate_translate/main.cpp
@@ -0,0 +1,31 @@
+#include <GL/glut.h>
+float angel = 0;
+void display()
+{
+    glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+    glPushMatrix();
+        glTranslatef(0.6, 0, 0);
+        glRotatef(angel, 0, 1, 0);
+        glColor3f(0, 1, 0);
+        glutSolidTeapot( 0.3 );
+    glPopMatrix();
+    glPushMatrix();
+        glRotatef(angel, 0, 1, 0);
+        glTranslatef(0.6, 0, 0);
+        glColor3f(1, 0, 0);
+        glutSolidTeapot( 0.3 );
+    glPopMatrix();
+    glutSwapBuffers();
+    angel++;
+}
+int main(int argc, char* argv[] )
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB|GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week03");
+
+    glutDisplayFunc(display);
+    glutIdleFunc(display);
+
+    glutMainLoop();
+}
